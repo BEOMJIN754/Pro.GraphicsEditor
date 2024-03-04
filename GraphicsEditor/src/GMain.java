356a20a42d02a1ac32dc241e148ddffcf920ebca
BEOMJIN LEE <beomjin754@gmail.com>
2024-03-05 02:29:06 +0900

    교수님코드

---
+++ GraphicsEditor/src/GMain.java
@@ -0,0 +1,19 @@
+import javax.swing.JFrame;
+
+public class GMain {
+
+	public GMain() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+
+//		GMainFrame mainFrame = new GMainFrame();
+//		mainFrame.setVisible(true);
+
+		JFrame mainFrame = new JFrame();
+		mainFrame.setVisible(true);
+
+	}
+
+}
---
+++ GraphicsEditor/src/GMainFrame.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class GMainFrame extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+
+	public GMainFrame() {
+		this.setLocation(200, 100);
+		this.setSize(400, 600);
+	}
+}
