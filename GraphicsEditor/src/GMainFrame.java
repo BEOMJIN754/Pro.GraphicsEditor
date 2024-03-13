99c6609038eda2253d138d1c44c2c2551e5d486d
BEOMJIN LEE <beomjin754@gmail.com>
2024-03-13 16:15:37 +0900

    [0313]마우스이벤트넣기

--- GraphicsEditor/src/GMainFrame.java
+++ GraphicsEditor/src/GMainFrame.java
@@ -1,3 +1,8 @@
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.LayoutManager;
+
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 
 public class GMainFrame extends JFrame {
@@ -5,11 +10,36 @@ public class GMainFrame extends JFrame {
 	private static final long serialVersionUID = 1L;
 
 	private GMenuBar menuBar;
+	private GShapeToolBar shapeToolBar;
+
+	private GDrawingPanel drawingPanel; 
+	
 	public GMainFrame() {
+		this.setSize(400, 600);
+		
+	//	LayoutManager layoutManager = new FlowLayout();
+		//LayoutManager layoutManager = new CardLayout();
+		LayoutManager layoutManager = new BorderLayout();
+		//자식들으 ㅣ배치를 정리해준 것 셋 레이아웃 이라는 관리자를 만듦
+		//LayoutManager layoutManager = new BoxLayout(this.getContentPane(), BoxLayout.X_AXIS);
+		this.setLayout(layoutManager);
+		
 		this.menuBar = new GMenuBar();
 		this.setJMenuBar(this.menuBar);
 		
-		this.setLocation(200, 100);
-		this.setSize(400, 600);
+		this.drawingPanel = new GDrawingPanel();
+		this.add(drawingPanel, BorderLayout.CENTER);
+		
+
+		this.shapeToolBar = new GShapeToolBar(drawingPanel);
+		this.add(shapeToolBar,BorderLayout.NORTH);
+		
+	
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+				
+		
 	}
+	
+	
 }
--- GraphicsEditor/src/GMenuBar.java
+++ GraphicsEditor/src/GMenuBar.java
@@ -6,10 +6,24 @@ public class GMenuBar extends JMenuBar {
 
 	public GFileMenu fileMenu;
 
+	private GFileMenu editMenu;
+	
+
 	public GMenuBar() {
 
-		this.fileMenu = new GFileMenu("file");
+		this.fileMenu = new GFileMenu("File");
+		this.editMenu = new GFileMenu("Edit");
 
 		this.add(this.fileMenu);
+		this.add(this.editMenu);
+		
+		fileMenu.add(new GMenuItem("tool"));
+		fileMenu.add(new GMenuItem("quit"));
+		fileMenu.add(new GMenuItem("setting"));
+		
+		editMenu.add(new GMenuItem("upload"));
+		editMenu.add(new GMenuItem("download"));
+		
+		
 	}
 }
