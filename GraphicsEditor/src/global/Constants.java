235d4bbfb295610e1ed822c627dd1a0eb28e7cf5
BEOMJIN LEE <beomjin754@gmail.com>
2024-03-26 13:38:01 +0900

    add oval

--- GraphicsEditor/src/global/Constants.java
+++ GraphicsEditor/src/global/Constants.java
@@ -1,19 +1,20 @@
 package global;
 
+import shapetools.GOval;
 import shapetools.GRectangle;
 import shapetools.GShape;
 
 public class Constants {
 	public enum EShapeButtons {
 		eRactangle("rectangle", new GRectangle()), // 스펠링 안틀릴 수 있음 
-		eOval("oval", new GRectangle()),
+		eOval("oval", new GOval()),
 		eLine("line", new GRectangle()), 
 		ePolygon("polygon", new GRectangle());
 	
 		private String text;
 		private String string;
-		private GRectangle shapeTool;
-		EShapeButtons(String text, GRectangle shapeTool){
+		private GShape shapeTool;
+		EShapeButtons(String text, GShape shapeTool){
 			this.text = text;
 			this.shapeTool = shapeTool;
 		}
--- GraphicsEditor/src/shapetools/GOval.java
+++ GraphicsEditor/src/shapetools/GOval.java
@@ -17,8 +17,8 @@ private Graphics graphics;
 		Graphics2D graphics2D = (Graphics2D) graphics;
 		graphics2D.setXORMode(graphics2D.getBackground());
 		//erase old shape 
-		graphics.drawRect(x1, y1, ox2-x1, oy2-y1);
+		graphics.drawOval(x1, y1, ox2-x1, oy2-y1);
 		//draw new shape 
-		graphics.drawRect(x1, y1, x2-x1, y2-y1);
+		graphics.drawOval(x1, y1, x2-x1, y2-y1);
 	}
 }
